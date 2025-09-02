826d12341cc16960e1dd2f75ff8db2b326938d9f
Андрей Полчихин <Polchihin@yandex.ru>
2025-09-03 04:10:36 +0500

    Изменение структуры проекта

--- XsltConverter/Windows/WindowAddItemInFile.xaml.cs
+++ XsltConverter/Windows/WindowAddItemInFile.xaml.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using XsltConverter.Classes;
+using XsltConverter.Models;
 
 namespace XsltConverter.Windows
 {
